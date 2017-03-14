61a89ed11f2f7d2916f132022e47e8712f7365dc
Vadim <vadosik.chumack@yandex.ru>
2017-03-14 20:26:21 +0200

    Crete service for Request

---
+++ leavedays/Models/EditModel/EditRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace leavedays.Models.EditModel
+{
+    public class EditRequest
+    {
+        public int UserId { get; set; }
+        public int CompanyId { get; set; }
+        public string Status { get; set; }
+        public string RequestBase { get; set; }
+        public DateTime[] VacationDates { get; set;}
+    }
+}
---
+++ leavedays/Services/RequestService.cs
@@ -0,0 +1,35 @@
+﻿using leavedays.Models;
+using leavedays.Models.EditModel;
+using leavedays.Models.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace leavedays.Services
+{
+    public class RequestService
+    {
+        private readonly IRequestRepository requestRepository;
+        RequestService(IRequestRepository requestRepository)
+        {
+            this.requestRepository = requestRepository;
+        }
+
+        public void Save(EditRequest editRequest)
+        {
+            Request request = new Request
+            {
+                UserId = editRequest.UserId,
+                CompanyId = editRequest.CompanyId,
+                Status = editRequest.Status,
+                RequestBase = editRequest.RequestBase,
+                SigningDate = DateTime.Now,
+                IsAccepted = false
+            };
+            requestRepository.Save(request);
+        }
+
+    }
+}

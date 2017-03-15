790aa544a9142efa68f4140d350e71de2f093748
Dima Kulchitskyi <prodigy@xaker.ru>
2017-03-15 14:59:09 +0200

    added some parts of auth

--- leavedays/Models/User.cs
+++ leavedays/Models/User.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace leavedays.Models
 {
-    public class User
+    public class User : IUser<int>
     {
         public virtual int Id { get; set; }
         public virtual string UserName { get; set; }
+        public virtual string Password { get; set; }
         public virtual int CompanyId { get; set; }
         public virtual string FirstName { get; set; }
         public virtual string LastName { get; set; }
         public virtual IEnumerable<SiteGroup> Groups { get; set; }
         public virtual IEnumerable<Module> Modules { get; set; }
+
+        public virtual int AccessFailedCount { get; set; }
+        public virtual bool LockoutEnabled { get; set; }
+        public virtual DateTimeOffset LockoutEnd { get; set; }
     }
 }
